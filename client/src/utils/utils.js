fe1c257f8db28fa4e6e3040256c06432e62664f4
stephenmarold <stephen.marold@gmail.com>
2025-05-11 18:23:01 -0400

    working, tested prototype.

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -2,6 +2,7 @@ export const calculateParticipantScores = (participants, vehicles) => {
 	if (!participants || !vehicles) {
 		return [];
 	}
+
 	for (let i = 0; i < participants.length; i++) {
 		const pick_1 = vehicles.find((vehicle) => {
 			return vehicle.vehicle_number == participants[i].driver_1;
@@ -12,11 +13,12 @@ export const calculateParticipantScores = (participants, vehicles) => {
 		const pick_3 = vehicles.find((vehicle) => {
 			return vehicle.vehicle_number == participants[i].driver_3;
 		});
-		participants[i].cur_points =
-			pick_1?.running_position ??
-			0 + pick_2?.running_position ??
-			0 + pick_3?.running_position ??
-			0;
+
+		// Previously wasn't working. Will revisit.
+		participants[i].cur_points = 0;
+		participants[i].cur_points += pick_1?.running_position ?? 0;
+		participants[i].cur_points += pick_2?.running_position ?? 0;
+		participants[i].cur_points += pick_3?.running_position ?? 0;
 	}
 	const sortedParticipants = [...participants].sort(
 		(a, b) => a.cur_points - b.cur_points
